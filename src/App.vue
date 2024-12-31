bd6d89dce860037c6e84efedac00d7145bfc049d
MikuAddict <gumingdebaka@gmail.com>
2024-12-31 17:44:49 +0800

    feat: 重构项目并添加新功能
    - 更新项目结构，移除 vue-cli，引入 vite
    - 添加 element-plus 组件库
    - 新增路由功能
    - 更新 App.vue，添加导航链接和路由视图
    - 移除 public/index.html
    - 更新 .gitignore 文件

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,31 @@
 <template>
-  <div>hello vue</div>
+  <div>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view></router-view>
 </template>
 
-
-
-<script>
-export default {
-  
+<style lang="scss" scoped>
+@import url('./assets/css/base.css');
+#app{
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
-</script>
 
+#nav{
+  padding: 30px;
 
-<style scoped>
+  a{
+    font-weight: bold;
+    color: #2c3e50;
 
+    &.router-link-exact-active{
+      color: #42b983;
+    }
+  }
+}
 </style>
---
+++ src/router/index.js
@@ -0,0 +1,44 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+    {
+      path: "/",
+      redirect: "/home",
+    },
+    {
+      path: "/:catchAll(.*)",
+      name: "404",
+      component: () => import("../views/404.vue"),
+    },
+    {
+      path: "/login",
+      name: "Login",
+      component: () => import("../views//login/Login.vue"),
+    },
+    {
+      path: "/category",
+      name: "Category",
+      component: () => import("../views/category/Category.vue"),
+    },
+    {
+      path:"/detail",
+      name:"Detail",
+      component:()=>import("../views/detail/Detail.vue")
+    },
+    {
+      path:"/shopcart",
+      name:"Shopcart",
+      component:()=>import("../views/shopcart/Shopcart.vue")
+    },
+    {
+      path:"/profile",
+      name:"Profile",
+      component:()=>import("../views/profile/Profile.vue")
+    }
+  ]
+  
+  const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes: routers.routes,
+  });
+export default router;
