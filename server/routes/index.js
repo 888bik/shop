8ef76b00c6b77dd3b8e1dbd68d02d924a9b62c14
MikuAddict <gumingdebaka@gmail.com>
2025-01-01 22:57:15 +0800

    feat(server): 添加 MySQL 数据库连接并迁移分类数据
    - 新增 MySQL 数据库连接文件 (db/sql.js)
    - 在项目中添加 mysql 依赖
    - 移除后端 API 调用，直接在前端使用静态分类数据
    - 更新前端分类组件，使用静态数据替代 API 请求

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,59 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var connection = require('../db/sql.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/api/index', function(req, res, next) {
-  res.send({
-    code: 0,
-    data: {
-      categoryList: [
-        {
-          id: 1,
-          imgUrl: 'src/assets/image/category/推荐-反推荐.png',
-          title: '推荐'
-        },
-        {
-          id: 2,
-          imgUrl: 'src/assets/image/category/电商彩妆家具电子产品.png',
-          title: '推荐'
-        },
-        {
-          id: 3,
-          imgUrl: 'src/assets/image/category/衣服,服装.png',
-          title: '服装'
-        },
-        {
-          id: 4,
-          imgUrl: 'src/assets/image/category/回流区日用百货.png',
-          title: '日用百货'
-        },
-        {
-          id: 5,
-          imgUrl: 'src/assets/image/category/手机.png',
-          title: '手机'
-        },
-        {
-          id: 6,
-          imgUrl: 'src/assets/image/category/洗衣机.png',
-          title: '家电'
-        },
-        {
-          id: 7,
-          imgUrl: 'src/assets/image/category/电脑.png',
-          title: '电脑'
-        },
-        {
-          id: 8,
-          imgUrl: 'src/assets/image/category/汉堡.png',
-          title: '食物'
-        }
-      ]
-    }
-  });
-});
+
 
 module.exports = router;
--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -5,7 +5,7 @@
     <div class="category-list">
         <ul>
             <li v-for="(item, index) in categoryList" :key="item.id">
-                <img :src="item.imgUrl" alt=""/>
+                <img :src="item.imgUrl" :alt="item.title"/>
                 <span>{{ item.title }}</span>
             </li>
         </ul>
@@ -23,23 +23,52 @@ export default {
         NavBar
     },
     setup() {
-        const categoryList = ref([]);
-
-        const fetchData = async () => {
-            try {
-                const res = await axios.get('/api/index');
-                if (res.data && res.data.code === 0) {
-                    categoryList.value = res.data.data.categoryList;
-                } else {
-                    console.error('API 请求失败:', res.data);
-                }
-            } catch (error) {
-                console.error('请求过程中出现错误:', error);
+        const categoryList = ref([
+            {
+              id: 1,
+              imgUrl: '/src/assets/image/category/推荐-反推荐.png', // 确保路径正确
+              title: '推荐'
+            },
+            {
+              id: 2,
+              imgUrl: '/src/assets/image/category/电商彩妆家具电子产品.png',
+              title: '电子产品'
+            },
+            {
+              id: 3,
+              imgUrl: '/src/assets/image/category/衣服,服装.png',
+              title: '服装'
+            },
+            {
+              id: 4,
+              imgUrl: '/src/assets/image/category/回流区日用百货.png',
+              title: '日用百货'
+            },
+            {
+              id: 5,
+              imgUrl: '/src/assets/image/category/手机.png',
+              title: '手机'
+            },
+            {
+              id: 6,
+              imgUrl: '/src/assets/image/category/洗衣机.png',
+              title: '家电'
+            },
+            {
+              id: 7,
+              imgUrl: '/src/assets/image/category/电脑.png',
+              title: '电脑'
+            },
+            {
+              id: 8,
+              imgUrl: '/src/assets/image/category/汉堡.png',
+              title: '食物'
             }
-        };
+        ]);
 
+        // 打印 categoryList 到控制台以调试
         onMounted(() => {
-            fetchData();
+            console.log('Category List:', categoryList.value);
         });
 
         return {
