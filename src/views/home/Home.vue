8a4cdd3b1dfa6172ef0f64f37921433dfa20201d
Bik <2829568515@qq.com>
2025-01-02 13:41:23 +0800

    标签商品列表展示

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -12,13 +12,26 @@
   <div class="recommend-list">
     <recommend></recommend>
   </div>
+  <!-- 分割线 -->
+  <div class="divider"></div>
+  <!-- 标签商品列表 -->
+  <div class="tab-goods">
+    <tab-goods></tab-goods>
+  </div>
 </template>
 
 <script setup>
 import NavBar from "@/components/common/navbar/NavBar.vue";
 import Banner from "@/views/home/cpns/Banner.vue";
 import Recommend from "@/views/home/cpns/Recommend.vue";
-import recommendData from '@/assets/data/recommendData.js';
+import TabGoods from "@/views/home/cpns/TabGoods.vue";
+import recommendData from "@/assets/data/recommendData.js";
 </script>
 
-<style scoped></style>
+<style scoped>
+.divider {
+  width: 100%;
+  height: 10px; /* 控制分割线的厚度 */
+  background-color:#eee; /* 控制分割线的颜色 */
+}
+</style>
---
+++ src/views/home/cpns/TabGoods.vue
@@ -0,0 +1,21 @@
+<template>
+  <van-tabs v-model:active="active" @click-tab="onClickTab" swipeable>
+    <van-tab title="电子产品">内容 1</van-tab>
+    <van-tab title="服装饰品">内容 2</van-tab>
+    <van-tab title="休闲零食">内容 3</van-tab>
+    <van-tab title="日用百货">内容 4</van-tab>
+  </van-tabs>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const active = ref(0);
+function onClickTab() {
+  console.log(active.value);
+  //TODO
+  // fetchTabGoodsByIndex();
+}
+</script>
+
+<style lang="scss" scoped></style>
